381fdf095743541c255bacaf2991d32952a26289
Jac22 <jacopo.prudentino@gmail.com>
2023-01-11 18:19:40 +0100

    Aggiunta possibile esclusione caratteri ripetuti

--- index.php
+++ index.php
@@ -9,19 +9,24 @@
     session_start();
         require __DIR__ . './libs/helper.php';
         $pswdLength=$_GET['pswd-input'] ?? false;
+        $repChar=$_POST['repChar'] ?? false;
     ?>
 </head>
 <body>
     <form>
         <label for="pswd-input">Quanto lunga vuoi la password? (Min 4 caratteri)</label>
         <input type="text" name='pswd-input'>
+        <label for="repChar">Caratteri ripetuti?</label>
+        <input type="radio" name='repChar' value='y'
+        <?php if(!$repChar) echo 'checked'?>
+        >
+        <input type="radio" name='repChar' value='n'>
         <input type="submit" value='Genera'>
     </form>
     <?php
         $pswdLength=intval($pswdLength);
         if($pswdLength && $pswdLength>=4){
-            $_SESSION['newPswd'] = str_shuffle(generatePswd($pswdLength));
-            var_dump($_SESSION['newPswd']);
+            $_SESSION['newPswd'] = str_shuffle(generatePswd($pswdLength, $repChar));
             header('Location: ./result.php');
         }
     ?>
--- libs/helper.php
+++ libs/helper.php
@@ -1,6 +1,7 @@
 <?php
+    session_start();
 
-    function generatePswd($length){
+    function generatePswd($length, $rep){
         $charUpArray=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'U', 'W', 'X', 'Y', 'Z'];
         $charLowArray=['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
         $charNumArray=['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
@@ -11,16 +12,40 @@
         $nNum=rand(1, ($length - 1 - $nUp - $nLow));
         $nSpec=$length - $nUp - $nLow - $nNum;
         for($i=0; $i<$nUp; $i++){
-            $pswd.=$charUpArray[rand(0, 25)];
+            $char=$charUpArray[rand(0, 25)];
+            if($rep=='y' && str_contains($pswd, $char)){
+                $i--;
+            }
+            else{
+                $pswd.=$char;
+            }
         }
         for($i=0; $i<$nLow; $i++){
-            $pswd.=$charLowArray[rand(0, 25)];
+            $char=$charLowArray[rand(0, 25)];
+            if($rep=='y' && str_contains($pswd, $char)){
+                $i--;
+            }
+            else{
+                $pswd.=$char;
+            }
         }
         for($i=0; $i<$nNum; $i++){
-            $pswd.=$charNumArray[rand(0, 9)];
+            $char=$charNumArray[rand(0, 9)];
+            if($rep=='y' && str_contains($pswd, $char)){
+                $i--;
+            }
+            else{
+                $pswd.=$char;
+            }
         }
         for($i=0; $i<$nSpec; $i++){
-            $pswd.=$charSpecArray[rand(0, 11)];
+            $char=$charSpecArray[rand(0, 10)];
+            if($rep=='y' && str_contains($pswd, $char)){
+                $i--;
+            }
+            else{
+                $pswd.=$char;
+            }
         }
         return $pswd;
     }
